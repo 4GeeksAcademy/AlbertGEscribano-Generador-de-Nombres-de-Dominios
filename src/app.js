f0f1b5461239e36c94b45109b2939a1f2d2d9a82
Albert González Escribano <126629570+AlbertgEscribano@users.noreply.github.com>
2023-05-17 02:52:19 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,59 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "our", "some", "any", "another", "super"];
+  let adjectives = [
+    "great",
+    "big",
+    "best",
+    "cool",
+    "funny",
+    "awesome",
+    "random",
+    "super",
+    "incredible",
+    "brutal"
+  ];
+  let nouns = [
+    "racoon",
+    "jogger",
+    "website",
+    "webapp",
+    "blog",
+    "hub",
+    "page",
+    "web",
+    "site",
+    "app",
+    "control"
+  ];
+  let tlds = [
+    ".com",
+    ".de",
+    ".cat",
+    ".dog",
+    ".space",
+    ".org",
+    ".rocks",
+    ".eu",
+    ".fr",
+    ".us"
+  ];
+
+  let domainNames = [];
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  document.querySelector("#generator").onclick = () => {
+    document.querySelector("#domain").innerHTML = `<h1>${
+      domainNames[Math.floor(Math.random() * domainNames.length)]
+    }</h1>`;
+  };
 };
